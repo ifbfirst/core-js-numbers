94d877a458a1738d313348f97d3c62e76465e3e3
Kate Mashko <katika.life@gmail.com>
2023-12-25 10:16:05 +0300

    feat:add solutions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -268,8 +268,16 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0) return 0;
+  if (index === 1 || index === 2) return 1;
+  let x = 1;
+  let y = 1;
+  for (let i = 2; i < index; i += 1) {
+    y += x;
+    x = y - x;
+  }
+  return y;
 }
 
 /**
